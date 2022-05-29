2874a1209933ed29110e9f33a05bd88368cd5434
Matias Carrizo <matias-carrizo@outlook.com>
2022-05-29 17:52:38 -0300

    pagina login terminada

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
+// Variables
 const iconoMenu = document.querySelector('.header__nav__iconoMenu');
 const menu = document.querySelector('.header__nav__menu')
-iconoMenu.addEventListener('click', () => {
-  menu.classList.toggle('--menuVisible');
-});
+const descargarApp = document.querySelector('#menuDescargarApp');
+
+// Funcion para cerrar el menu segun el elemento que se cliquee
+const cerrarMenu = (elemento) => {
+  elemento.addEventListener('click', () => {
+    menu.classList.toggle('--menuVisible');
+  });
+}
+cerrarMenu(iconoMenu);
+cerrarMenu(descargarApp);
