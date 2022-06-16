2d2d72df9df58ffe8904d409249addcac7038d1b
Juano <jivila@outlook.com>
2022-06-16 01:11:57 -0300

    Agregado mensaje en consola de la api | Pendiente de agregar el html en pantalla

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
-const dolarFetch = fetch(
-  "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
-)
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-  });
+"use strict";
+
+const dolarFetch = fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
+	.then((res) => res.json())
+	.then((data) => {
+		console.log(data);
+		data.forEach((element) => {
+			const casaDeCambio = element.casa;
+
+			console.log(
+				`Agencia:${casaDeCambio.agencia}\nTipo de moneda: ${casaDeCambio.nombre}\nPrecio de compra: $${casaDeCambio.compra}\nPrecio de venta $${casaDeCambio.venta}`
+			);
+
+			// Modificar console log por elementos
+			h1.innerHTML = "Hey";
+		});
+	})
+	.catch(function (error) {
+		console.log("Hubo un problema con la petición Fetch:" + error.message);
+	});
