785e57cf11cbab38bd2260e653809df67278c75f
Juano <jivila@outlook.com>
2022-06-17 17:46:00 -0300

    Pagina dolar finalizada

--- js/app.js
+++ js/app.js
@@ -1,33 +1,36 @@
 "use strict";
 
+
+const spinner = document.querySelector('#spinner')
+
 const dolarFetch = fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
 	.then((res) => res.json())
 	.then((data) => {
-		console.log(data);
 		data.forEach((element) => {
+            spinner.remove()
 			const { casa } = element;
-			if (casa.compra === "No Cotiza" || casa.venta === "0") return;
-			casa.variacion = (parseFloat(casa.venta) / parseFloat(casa.compra) - 1).toFixed(4) * 100;
+        //   ELiminando las casas que no operan
+			if (casa.venta === "0") return;
 
-			// Creating and inserting elements
-			const card = document.createElement("div");
-			const cardHTML = `		
-            <div class="card bg-light mb-5" style="max-width: 28rem">
+
+			const cointainerEl = document.querySelector("#card-col");
+
+			const html = `	<div class="col-xl-4 text-center">
+			<div class="card mx-5 my-4 text-center" style="max-width: 34rem; ">
 			<div class="card-header">Agencia: ${casa.agencia} | Moneda: ${casa.nombre}</div>
 			<div class="card-body">
-				<h5 class="card-title">Precio de compra: $${casa.compra}</h5>
+				<h5 class="card-title">Precio de compra: ${casa.compra !== "No Cotiza" ? "$"+casa.compra : "no cotiza"}</h5>
 				<p class="card-text">
 				<h5 class="card-title">Precio de venta: $${casa.venta}</h5>
 				<p class="card-text">
-				<h5 class="card-title">Variacion del: ${casa.variacion}%</h5>
+				<h5 class="card-title">${casa.variacion ? "Variación: " + casa.variacion + "%" : "Variación no disponible"}</h5>
 				<p class="card-text">
 				</p>
 			</div>
-		    </div> `;
+			</div>
+			</div>`;
 
-			card.innerHTML = cardHTML;
-
-			document.body.appendChild(card);
+			cointainerEl.insertAdjacentHTML("afterend", html);
 		});
 	})
 	.catch(function (error) {
